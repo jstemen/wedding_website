8877af4f585df9f947a3bd131f06d8d23afadaf7
Jared Stemen <jstemen@gmail.com>
2014-11-16 13:49:49 -0800

    Resizxing esctions to be the greater of the sorignal content size or the device screen size.

--- app/assets/javascripts/wedding_app.js
+++ app/assets/javascripts/wedding_app.js
@@ -1,12 +1,20 @@
+const NAV_HEIGHT = 30
+
 function scrollToId(id) {
     $('html, body').animate({
-        scrollTop: $(id).offset().top - 30
+        scrollTop: $(id).offset().top - NAV_HEIGHT
     }, 500);
 }
 
 
 $(window).resize(function () {
-    $('.content-section').height($(window).height() - 30);
+
+    $('.content-section').each( function(index){
+        var fullScreenSize = $(window).height() - NAV_HEIGHT;
+        var currentDivSize = $(this).height();
+        var rightSize = Math.max(fullScreenSize, currentDivSize)
+        $(this).height(rightSize);
+    })
 });
 
 $(window).trigger('resize');
