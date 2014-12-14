ad6d9a4ad58ee6f8abf2fd773971309be063cf41
Jared Stemen <jstemen@gmail.com>
2014-12-14 13:33:06 -0800

    Adding win with shoes condition

--- app/assets/javascripts/starstruck.js
+++ app/assets/javascripts/starstruck.js
@@ -41,7 +41,7 @@ function loadGame() {
             this.player;
 
             function Player(x, y) {
-
+                this.hasShoes=false;
                 Phaser.Sprite.call(this, game, x, y, 'dude', 1)
                 game.physics.enable(this, Phaser.Physics.ARCADE);
 
@@ -81,17 +81,16 @@ function loadGame() {
                     player.myKill();
                 }
                 player.body.velocity.x = 0;
-                if (player.x > 3183) {
+                if (player.x > 3183 && player.hasShoes) {
                     printMsg("The save the date is September 26th 2015!".toUpperCase())
                 }
 
                 if (player.y > game.height - 20 && player.alive) {
                     player.kill();
                 }
-
-
+                const horizSpeed = 500;
                 if (cursors.left.isDown) {
-                    player.body.velocity.x = -150;
+                    player.body.velocity.x = horizSpeed * -1;
 
                     if (facing != 'left') {
                         player.animations.play('left');
@@ -99,7 +98,7 @@ function loadGame() {
                     }
                 }
                 else if (cursors.right.isDown) {
-                    player.body.velocity.x = 150;
+                    player.body.velocity.x = horizSpeed;
 
                     if (facing != 'right') {
                         player.animations.play('right');
@@ -290,14 +289,15 @@ function loadGame() {
 
         function update() {
 
-            //console.log(player.x)
             $.each(EnemyModule.enemies, function (index, enemy) {
                 game.physics.arcade.collide(PlayerModule.player, enemy, collisionHandler);
             });
 
             game.physics.arcade.collide(shoes, layer);
-
-
+            game.physics.arcade.collide(PlayerModule.player, shoes, function(player, shoes){
+                shoes.destroy();
+                player.hasShoes=true
+            });
         }
 
         function render() {
