4863fc59fc3941e145166c49726baf5751eb29d6
Jared Stemen <jstemen@gmail.com>
2015-01-31 14:55:23 -0800

    adding constriants ot invenitaions and guests

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,6 @@
 class Guest < ActiveRecord::Base
   belongs_to :invitation
+  validates :first_name, :last_name, presence: true
 
   validates :email_address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, allow_blank: true
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,7 @@
 class Invitation < ActiveRecord::Base
   has_and_belongs_to_many :events
   has_many :guests
-  
+
+  validates :code, presence: true
+  validates :max_guests, numericality: true, presence: true
 end
