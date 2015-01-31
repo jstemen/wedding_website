1f2527cf177a57007e63b094ccff11a4ef63786b
Jared Stemen <jstemen@gmail.com>
2015-01-31 14:37:14 -0800

    allow blank email

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ActiveRecord::Base
   belongs_to :invitation
 
-  validates :email_address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :email_address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, allow_blank: true
 end
