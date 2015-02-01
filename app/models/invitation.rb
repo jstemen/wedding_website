908addadde4c7ee20fcbbcd86a9ee4579bff10ec
Jared Stemen <jstemen@gmail.com>
2015-01-31 19:07:06 -0800

    restricting number of guesets to max_gtess

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,7 +1,24 @@
+class InvitationMustBeLessThanMax < ActiveModel::Validator
+  def validate(record)
+    max_guests = record.max_guests || 0
+    if record.guests.size > max_guests
+      raise ActiveRecord::RecordInvalid "You've exceeded your max number of allowed guest: #{max_guests}"
+    end
+  end
+end
+
 class Invitation < ActiveRecord::Base
+  include ActiveModel::Validations
   has_and_belongs_to_many :events
   has_many :guests
-
+  validate :guests, :invitation_must_be_less_than_max
   validates :code, presence: true
   validates :max_guests, numericality: true, presence: true
+  
+  def invitation_must_be_less_than_max
+    max_guests = self.max_guests || 0
+    if guests.size > max_guests
+      errors.add(:guests, "You've exceeded your max number of allowed guest: #{max_guests}")
+    end
+  end
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -17,8 +17,24 @@ RSpec.describe Invitation, :type => :model do
   it "must not allow string max_guests" do
     expect { create(:invitation, {max_guests: 'dog'}) }.to raise_error ActiveRecord::RecordInvalid
   end
+
+  it "must not allow more guests than max_guests" do
+    max_guests = 2
+    invitation = build(:invitation, max_guests: max_guests)
+    (max_guests +5).times {
+      invitation.guests << build(:guest)
+    }
+    expect{invitation.save!}.to raise_error(ActiveRecord::RecordInvalid)
+  end
   
-  
+  it "allows guests to max_guests" do
+    max_guests = 2
+    invitation = build(:invitation, max_guests: max_guests)
+    (max_guests).times {
+      invitation.guests << build(:guest)
+    }
+    invitation.save!
+  end
 
   it "has max_guests" do
     invitation = create :invitation
