87a0d01343235cbac31879370f30b547c98ee522
Jared Stemen <jstemen@gmail.com>
2015-02-05 21:35:24 -0800

    starting to make guets select page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :guests
 
   resources :invitation_groups
-  get 'invitation_groups/show/:code/', to: 'invitation_groups#show'
+  get 'invitation_groups/show/:code/guests', to: 'invitation_groups#show'
+  get 'invitation_groups/show/:code/events', to: 'invitation_groups#show_events'
 
 end
