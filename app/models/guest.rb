cc3d4e64e3736fc6a1d75882a0879fc605144e99
Jared Stemen <jstemen@gmail.com>
2015-02-05 21:45:59 -0800

    adding full name and creating a select box equal to max number of guests

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,4 +3,8 @@ class Guest < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
 
   validates :email_address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, allow_blank: true
+  
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
