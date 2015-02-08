63bf7c3db590c1c8a3f7553c1d4a382243a65c42
Jared Stemen <jstemen@gmail.com>
2015-02-08 14:59:02 -0800

    adding first couple of tests for the RSVP process

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -2,15 +2,28 @@
 #   As a visitor
 #   I want to visit a home page
 #   So I can learn more about the website
-feature 'Home page' do
+feature 'Guest RSVPs' do
 
+  def add_guests(code)
+    "/invitation_groups/show/#{code}/guests"
+  end
+  
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
   #   Then I see "Welcome"
-  scenario 'visit the home page' do
+  scenario 'by visiting the home page and entering a invitation code' do
     visit root_path
-    expect(page).to have_content 'Save the Date Game'
+    invitation_group = create :invitation_group
+    fill_in('code', :with => invitation_group.code)
+    click_button 'Lookup'
+    expect(page).to have_content 'Please Enter The Guests That Will Be Coming:'
+  end
+
+  scenario 'and adds a new user to their invitation group' do
+    invitation_group = create :invitation_group
+    visit add_guests(invitation_group.code)
+    expect(page).to have_content 'Please Enter The Guests That Will Be Coming:'
   end
 
 end
