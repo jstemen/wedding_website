fe149a8f6790c4c49eb52a7b391e8dcac5904af2
Jared Stemen <jstemen@gmail.com>
2015-02-08 19:55:34 -0800

    adding tes for adding user

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -3,11 +3,30 @@
 #   I want to visit a home page
 #   So I can learn more about the website
 feature 'Guest RSVPs' do
+  scenario 'and adds a new user to their invitation group' do
+    invitation_group = create :invitation_group
+    guest = create :guest
+    invitation_group.guests << guest
+    invitation_group.save!
+    visit add_guests(invitation_group.code)
+    before_count = invitation_group.guests.size
+    out = find(:xpath, "//form/ul/li[last()]")
+    within(:xpath, "//form/ul/li[last()]") do
+      fill_in 'First name', :with => 'Jared'
+      fill_in 'Last name', :with => 'Stemen'
+    end
+    click_button 'Update Invitation group'
+    expect(page).to have_content 'Please Enter The Guests That Will Be Coming:'
+
+    after_count = invitation_group.guests.size
+
+    expect(before_count+1).to be(after_count)
+  end
 
   def add_guests(code)
     "/invitation_groups/show/#{code}/guests"
   end
-  
+
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
@@ -20,10 +39,5 @@ feature 'Guest RSVPs' do
     expect(page).to have_content 'Please Enter The Guests That Will Be Coming:'
   end
 
-  scenario 'and adds a new user to their invitation group' do
-    invitation_group = create :invitation_group
-    visit add_guests(invitation_group.code)
-    expect(page).to have_content 'Please Enter The Guests That Will Be Coming:'
-  end
 
 end
