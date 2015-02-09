0eb72c03faab42faa5d51b89f29eecb2c203f538
Jared Stemen <jstemen@gmail.com>
2015-02-08 20:31:23 -0800

    clean up

---
+++ spec/controllers/invitation_group_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe InvitationGroupsController do
+
+  it "should be successful" do
+
+
+    {"guests_attributes" => {"0" => {"first_name" => "bo", "last_name" => "dole", "id" => "1"},
+                             "1" => {"first_name" => "bo", "last_name" => "dole", "id" => "2"},
+                             "2" => {"first_name" => "jared", "last_name" => "stemen", "id" => "3"},
+                             "3" => {"first_name" => "tom", "last_name" => "bar", "id" => "4"},
+                             "4" => {"first_name" => "fo", "last_name" => "bar", "id" => "5"},
+                             "5" => {"first_name" => "Palak", "last_name" => "Shah", "id" => "6"}},
+     "invitations" => {"1" => {"invitation_attributes" => {"guest_ids" => ["3"]}},
+                       "2" => {"invitation_attributes" => {"guest_ids" => ["2", "4"]}}}}
+    get :index
+    response.should be_successful
+  end
+
+end
