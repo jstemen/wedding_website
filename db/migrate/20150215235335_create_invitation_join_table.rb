3d9877bbff40d6a3a3a37b349d76d065073ca428
Jared Stemen <jstemen@gmail.com>
2015-02-15 18:05:31 -0800

    removing unique constraint

--- db/migrate/20150215235335_create_invitation_join_table.rb
+++ db/migrate/20150215235335_create_invitation_join_table.rb
@@ -2,7 +2,7 @@ class CreateInvitationJoinTable < ActiveRecord::Migration
   def change
     create_join_table :guests, :invitations do |t|
       # t.index [:guest_id, :invitation_id]
-      t.index [:invitation_id, :guest_id], unique: true
+      t.index [:invitation_id, :guest_id]
     end
   end
 end
