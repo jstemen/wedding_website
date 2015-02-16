1f2322faf96889867add76588819adbe0bf62f48
Jared Stemen <jstemen@gmail.com>
2015-02-15 16:14:38 -0800

    linking guests and invitations with join table

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ActiveRecord::Base
-  belongs_to :invitation
+  has_and_belongs_to_many :invitations
   validates :first_name, :last_name, presence: true
 
   validates :email_address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, allow_blank: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205053912) do
+ActiveRecord::Schema.define(version: 20150215235335) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -26,13 +26,18 @@ ActiveRecord::Schema.define(version: 20150205053912) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "invitation_id"
     t.integer  "invitation_group_id"
   end
 
   add_index "guests", ["email_address"], name: "index_guests_on_email_address", unique: true
   add_index "guests", ["invitation_group_id"], name: "index_guests_on_invitation_group_id"
-  add_index "guests", ["invitation_id"], name: "index_guests_on_invitation_id"
+
+  create_table "guests_invitations", id: false, force: true do |t|
+    t.integer "guest_id",      null: false
+    t.integer "invitation_id", null: false
+  end
+
+  add_index "guests_invitations", ["invitation_id", "guest_id"], name: "index_guests_invitations_on_invitation_id_and_guest_id", unique: true
 
   create_table "invitation_groups", force: true do |t|
     t.string   "code"
