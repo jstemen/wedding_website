6b1e5a69e83fcba5ac50827371359c97b4f0daa2
Jared Stemen <jstemen@gmail.com>
2015-02-20 22:49:25 -0800

    converting Capybara tests to rspec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,14 +1,9 @@
-def expect_to_be_on_events_page
-  find("#eventsSelectionPage")
-end
 
-# Feature: Home page
-#   As a visitor
-#   I want to visit a home page
-#   So I can learn more about the website
-feature 'Guest RSVPs' do
+describe "The RSVP Process", :type => :feature do
 
-  scenario 'can see their existing RSVPs on the events page' do
+
+
+  it 'users can see their existing RSVPs on the events page' do
     invitation_group = create(:invitation_group, :five_guests, :four_invitations)
 
 
@@ -33,7 +28,7 @@ feature 'Guest RSVPs' do
   end
 
 
-  scenario 'can select guests' do
+  it 'users can select guests' do
     invitation_group = create(:invitation_group, :five_guests, :four_invitations)
 
     visit link_guests_to_events(invitation_group.code)
@@ -55,43 +50,15 @@ feature 'Guest RSVPs' do
 
   end
 
-
-  scenario 'and clicks "Continue To Events Page" link.  They are taken to the Events page' do
-    invitation_group = create :invitation_group
-    guest = create :guest
-    visit add_guests(invitation_group.code)
-    click_link 'events_page'
-    expect_to_be_on_events_page
-    expect(page).to have_content 'Please select which guests will be attending each event.'
-  end
-
-  scenario 'and adds a new user to their invitation group' do
-    invitation_group = create :invitation_group
-    guest = create :guest
-    visit add_guests(invitation_group.code)
-    submited_guests = []
-    submited_guests << fill_in_guest_submit("//form/ul/li")
-    4.times { submited_guests << fill_in_guest_submit("//form/ul/li[last()]") }
-
-    expect(page).to have_content 'Please Enter The Guests That Will Be Coming'
-
-    after_guests = invitation_group.guests
-    expect(after_guests.size).to eq(submited_guests.size)
-
-    redefine_equals after_guests, :last_name, :first_name
-
-    submited_guests.each { |guest|
-      expect(after_guests).to include(guest)
-    }
-  end
-
-  scenario 'by visiting the home page and entering a invitation code' do
+  it 'users can go to the events page by entering their invitation code on the home page' do
     visit root_path
     invitation_group = create :invitation_group
     fill_in('code', :with => invitation_group.code)
     click_button 'Lookup'
     expect_to_be_on_events_page
   end
+
+  private
 
   def redefine_equals(objs, *args)
     #redefine equals to only look at first and last name
@@ -131,6 +98,10 @@ feature 'Guest RSVPs' do
 
   def link_guests_to_events(code)
     "/invitation_groups/show/#{code}/events"
+  end
+
+  def expect_to_be_on_events_page
+    find("#eventsSelectionPage")
   end
 
 
