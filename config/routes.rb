a2b68b2d3264dcc2e0623aee26f4c7c09d01fde0
Jared Stemen <jstemen@gmail.com>
2015-02-21 21:38:11 -0800

    Upon clicking the "Confirm RSVP" button on the confirmation page, guests are taken to the thank-you page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :guests
 
   resources :invitation_groups
+  get 'invitation_groups/show/:code/thank_you', to: 'invitation_groups#thank_you', as: 'thank_you'
   get 'invitation_groups/show/:code/guests', to: 'invitation_groups#show', as: 'add_guests'
   get 'invitation_groups/show/:code/events', to: 'invitation_groups#show_events', as: 'link_guests_to_events'
 
