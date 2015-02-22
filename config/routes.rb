19a9b6b869554feb3edf3488d2c5f50cd2dc1920
Jared Stemen <jstemen@gmail.com>
2015-02-21 23:28:46 -0800

    adding content changes to home page and link to non indian's guide

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   get '/invitation_groups/confirmation', to: 'invitation_groups#confirmation'
   resources :invitations
   resources :guests
-
   resources :invitation_groups
-  get 'invitation_groups/show/:code/thank_you', to: 'invitation_groups#thank_you', as: 'thank_you'
-  get 'invitation_groups/show/:code/guests', to: 'invitation_groups#show', as: 'add_guests'
-  get 'invitation_groups/show/:code/events', to: 'invitation_groups#show_events', as: 'link_guests_to_events'
+  get 'pages/non_indian_guide',                 to: 'pages#non_indian_guide',        as: 'non_indian_guide'
+  get 'invitation_groups/show/:code/thank_you', to: 'invitation_groups#thank_you',   as: 'thank_you'
+  get 'invitation_groups/show/:code/guests',    to: 'invitation_groups#show',        as: 'add_guests'
+  get 'invitation_groups/show/:code/events',    to: 'invitation_groups#show_events', as: 'link_guests_to_events'
 
 end
