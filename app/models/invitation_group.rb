32f0ee916e8cf863f8be1f7e23a49311b99ef451
Jared Stemen <jstemen@gmail.com>
2015-02-24 09:26:26 -0800

    all tests seem to pass intermittently.  Need to investigate ids in html vs ids in db.

--- app/models/invitation_group.rb
+++ app/models/invitation_group.rb
@@ -2,13 +2,16 @@ class InvitationGroup < ActiveRecord::Base
   validates :is_confirmed, :inclusion => {:in => [true, false]}
   validates :code, presence: true
 
-  has_many :invitations,  dependent: :destroy
+  has_many :invitations, dependent: :destroy
 
   accepts_nested_attributes_for :invitations
 
   def has_attendees
-     guests = invitations.select(&:is_accepted).collect(&:guest).compact
-     not guests.empty?
+    not accepted_attendees.empty?
+  end
+
+  def accepted_attendees
+    invitations.select(&:is_accepted).collect(&:guest).compact
   end
 end
 
