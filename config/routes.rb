592da9b385bf63b04e9a3b30f64da44aba9b2732
Jared Stemen <jstemen@gmail.com>
2015-02-24 20:01:48 -0800

    Adding tests for render of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :guests
   resources :invitation_groups
   get 'pages/non_indian_guide',                 to: 'pages#non_indian_guide',        as: 'non_indian_guide'
+  get 'pages/save_the_date',                    to: 'pages#save_the_date',           as: 'save_the_date'
   get 'invitation_groups/show/:code/thank_you', to: 'invitation_groups#thank_you',   as: 'thank_you'
   get 'invitation_groups/show/:code/guests',    to: 'invitation_groups#show',        as: 'add_guests'
   get 'invitation_groups/show/:code/events',    to: 'invitation_groups#show_events', as: 'link_guests_to_events'
