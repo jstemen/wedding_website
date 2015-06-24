183942db5be766e3013089d3542084c66a540039
Jared Stemen <jstemen@gmail.com>
2015-06-23 18:06:25 -0700

    fixing test

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,21 +1,18 @@
 describe 'The admin process', :type => :feature do
 
-  it "A singed in admin should be able to access the invitations index page" do
-    pass = '123abc'
+  it "A signed in admin should be able to access the invitations index page" do
+    pass = '123abc' * 2
     admin = create :admin, {password: pass}
 
     visit invitation_groups_url
-    expect(page).to have_content('Total Invitation Group Count')
 
-=begin
-    invitation_group = create(:invitation_group, :five_guests)
-    invitation = invitation_group.invitations.first
-    invitation.is_accepted = true
-    invitation_group.save!
-    invitation.save!
-    visit link_thank_you(invitation_group.code)
-    expect_to_be_on_thank_you_page
-=end
+    expect(page).to have_content('Log in')
+
+    fill_in('admin_email', :with => admin.email)
+    fill_in('admin_password', :with => pass)
+    click_button 'Log in'
+
+    expect(page).to have_content('Total Invitation Group Count')
   end
 
 end
