a1cfdc9de13efe7c748acd5645a7dbadb075b895
Jared Stemen <jstemen@gmail.com>
2015-06-28 11:42:58 -0700

    moving error messages into the floating top

--- app/assets/javascripts/wedding_app.js
+++ app/assets/javascripts/wedding_app.js
@@ -32,3 +32,39 @@ $(window).ready(function () {
 $(document).on('page:load', function () {
     onChange()
 })
+
+function moveDiv(newParent, child) {
+    if (child.parent != newParent) {
+        newParent.append(child)
+    }
+}
+function isScrolledIntoView(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    return ((originalBottom <= docViewBottom) && (orginalTop >= docViewTop));
+}
+
+var originalParentsHash = {};
+var orginalTop = null;
+var originalBottom = null;
+$(window).scroll(function (event) {
+    var child = $(".followMe")
+
+    if (!originalParentsHash[child]) {
+        var elemTop = child.offset().top;
+        var elemBottom = elemTop + child.height();
+        originalBottom = elemBottom
+        orginalTop= elemTop
+        originalParentsHash[child] = child.parent();
+    }
+    //var scroll = $(window).scrollTop();
+    if (isScrolledIntoView(originalParentsHash[child])) {
+        moveDiv(originalParentsHash[child], child)
+    } else {
+        moveDiv($("#headContainer"), child)
+    }
+});
