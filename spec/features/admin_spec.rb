1f9f2f6c3d0f952e4b81e9ad0da27423caed70fd
Jared Stemen <jstemen@gmail.com>
2015-07-26 10:18:52 -0700

    Adding spec

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -15,22 +15,60 @@ describe 'The admin process', :type => :feature do
 
 
   it "A signed in admin should be able to access the invitations index page" do
-
     expect(page).to have_content('Total Invitation Group Count')
   end
 
-  it "We can delete invitations from the edit invitations page" do
+  it "allows an admin to create and inviation" do
+    invitation_group = create(:invitation_group, :five_guests)
+    guest = invitation_group.guests.sample
+    fresh_event = create(:event)
+    visit edit_invitations_path invitation_group.id
+    #save_and_open_page
+    node = find "#event_to_guests_#{guest.id}-#{fresh_event.id}"
+    node.set(true)
+    click_button 'Save'
+
+    invited = guest_invited_to_event? event: fresh_event, guest: guest
+    expect(invited).to be(true)
+  end
+
+  it "allows the admin to delete an invitation" do
+    invitation_group = create(:invitation_group, :five_guests)
+    inv_to_del = invitation_group.invitations.sample
+
+    visit edit_invitations_path invitation_group.id
+
+    guest = inv_to_del.guest
+    event = inv_to_del.event
+    node = find "#event_to_guests_#{guest.id}-#{event.id}"
+    node.set(false)
+    click_button 'Save'
+
+    invited = guest_invited_to_event? event: event, guest: guest
+    expect(invited).to be(false)
+  end
+
+  it "is presented with a set of checkboxes that is consistent with the database" do
     invitation_group = create(:invitation_group, :five_guests)
 
     visit edit_invitations_path invitation_group.id
 
-    Event.all { |event|
-      invitation_group.guests { |guest|
+    Event.all.each { |event|
+      invitation_group.guests.each { |guest|
         node = find("#event_to_guests_#{guest.id}-#{event.id}")
-        expect(node.value).to eq("10")
+        if guest_invited_to_event? guest: guest, event: event
+          expect(node).to be_checked
+        else
+          expect(node).to_not be_checked
+        end
       }
     }
 
+  end
+
+  def guest_invited_to_event?(event:, guest:)
+    size = Invitation.where(event: event, guest: guest).size
+    size > 0
   end
 
 end
