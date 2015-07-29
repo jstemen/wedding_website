6e125762aafa157acc8dea643b0c11d3246147b4
Jared Stemen <jstemen@gmail.com>
2015-07-28 20:06:08 -0700

    guest controller clean up

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,37 +1,43 @@
 class GuestsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_guest, only: [:show, :edit, :update, :destroy]
 
   def new
-    #params[:invitation_group_id]
     @guest = Guest.new
   end
 
   def create
-    perm= params.permit(guest: [:first_name, :last_name, :email_address]).tap do |p|
-      p[:guest][:invitation_group_id] = params[:invitation_group_id]
-    end
-    if Guest.new(perm[:guest]).save
-
-      id = perm['guest']['invitation_group_id']
+    @guest = Guest.new(guest_params[:guest])
+    if @guest.save
+      id = guest_params['guest']['invitation_group_id']
       redirect_to(edit_invitations_url(id), notice: 'Guest was successfully created.')
     else
-      @guest = Guest.new(perm[:guest])
       render :new
     end
   end
 
   def edit
-    @guest = Guest.find params[:id]
   end
 
   def update
-    perm= params.permit(guest: [:first_name, :last_name, :email_address])
-    @guest = Guest.find(params[:id])
-    if @guest.update perm['guest']
+    if @guest.update guest_params['guest']
       redirect_to(edit_invitations_url(params[:invitation_group_id]), notice: 'Guest was successfully updated.')
     else
       render :edit
     end
+  end
+
+  private
+
+  def guest_params
+    perm= params.permit(guest: [:first_name, :last_name, :email_address]).tap do |p|
+      p[:guest][:invitation_group_id] = params[:invitation_group_id]
+    end
+    perm
+  end
+
+  def set_guest
+    @guest = Guest.find params[:id]
   end
 
 end
