faf78cf4d8fdea5290d7c77584bebc80152100e2
Jared Stemen <jstemen@gmail.com>
2015-08-31 21:26:44 -0700

    getting rid of rspec warning

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe Invitation, :type => :model do
 
   it "may not have more than one event" do
     invitation = create(:invitation)
-    expect {invitation.events << create(:event)}.to raise_error
+    expect {invitation.events << create(:event)}.to raise_error NoMethodError
   end
 
   it "must have an event" do
     invitation = build(:invitation, event: nil)
-    expect {invitation.save!}.to raise_error
+    expect {invitation.save!}.to raise_error ActiveRecord::RecordInvalid
   end
 
   it "must have an invitation group" do
     invitation = build(:invitation, invitation_group: nil)
-    expect {invitation.save!}.to raise_error
+    expect {invitation.save!}.to raise_error ActiveRecord::RecordInvalid
   end
 
   
