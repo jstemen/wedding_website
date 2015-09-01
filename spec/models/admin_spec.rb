a65cdcd18d587f1ef92cc3b5bb1d8c1b9ee96283
Jared Stemen <jstemen@gmail.com>
2015-08-31 21:35:22 -0700

    adding some trivial admin specs

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Admin, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @admin = create(:admin)
+  end
+  it 'has an email' do
+    expect(@admin.email).not_to be(nil)
+  end
+  it 'has a password' do
+    expect(@admin.password).not_to be(nil)
+  end
 end
