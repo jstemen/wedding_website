45a53f6d30de54cf0eea013a687611bce8e60225
Jared Stemen <jstemen@gmail.com>
2015-09-01 20:18:18 -0700

    adding css and js compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,11 +39,11 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  #config.assets.js_compressor = :uglifier
-  #config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
